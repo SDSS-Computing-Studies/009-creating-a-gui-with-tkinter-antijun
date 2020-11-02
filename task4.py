0ea57af3bacb1cc527095b0f79a2ed4974cc29db
antijun <68279777+antijun@users.noreply.github.com>
2020-11-02 12:12:27 -0800

    .

--- task4.py
+++ task4.py
@@ -3,6 +3,7 @@ from tkinter import *
 
 window = tk.Tk()
 window.title("Example")
+window.geometry("400x200")
 
 dogp = PhotoImage(file="dog.png")
 
@@ -13,9 +14,9 @@ l3 = Label(window, text=" A cuddly little puppy! This is from the same ",
 l4 = Label(window, text="creators who brought you Keropi and Kero Kero",
            background="#aaffff")
 
-l1.grid(row=1, column=2, rowspan=3, columnspan=2)
-l2.grid(row=2, column=3, columnspan=2)
-l3.grid(row=4, column=2, columnspan=3)
-l4.grid(row=5, column=2, columnspan=3)
+l1.place(x=130, y=40)
+l2.place(x=190, y=75)
+l3.place(x=70, y=140)
+l4.place(x=60, y=160)
 
 window.mainloop()
